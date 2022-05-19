c21160ba199aecd0338330e31c2f08712ff0f0da
Kristian Starkloff <54065797+kristianstarkloffjedox@users.noreply.github.com>
2022-05-19 20:34:28 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="Playground",
+        description="Playground for tox",
+        author="Myself"
+    )
---
+++ tests/test_add.py
@@ -0,0 +1,5 @@
+def add(x, y):
+    return x+y
+
+def test_add():
+    assert(5, add(2,3))
